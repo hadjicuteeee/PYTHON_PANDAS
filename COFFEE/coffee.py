066a5ad63c3d64be3ca5fd8429627aad8c1925e0
hadjinxx <144666381+hadjicuteeee@users.noreply.github.com>
2025-10-14 16:57:24 +0800

    Update coffee.py

--- COFFEE/coffee.py
+++ COFFEE/coffee.py
@@ -8,17 +8,17 @@ df['Date'] = pd.to_datetime(df['Date'])
 df = df.rename(columns={'Cash Type': 'cash_type'})
 
 #Data Analysis
-#value = df['coffee_name'].value_counts()
-#print(value)
+value = df['coffee_name'].value_counts()
+print(value)
 
-#val = df['cash_type'].value_counts()
-#print(val)
+val = df['cash_type'].value_counts()
+print(val)
 
-#total_sum = df.groupby('coffee_name', as_index=True)['money'].sum()
-#print(total_sum)
+total_sum = df.groupby('coffee_name', as_index=True)['money'].sum()
+print(total_sum)
 
-#total_date = df.groupby('Date', as_index=False)['money'].sum()
-#print(total_date)
+total_date = df.groupby('Date', as_index=False)['money'].sum()
+print(total_date)
 
 top_5 = df.groupby('coffee_name', as_index=False)['money'].sum().sort_values(by='money', ascending=False).head(5)
 print("\n--- Top 5 Best-Selling Coffees ---")
@@ -26,4 +26,5 @@ print(top_5)
 
 total_revenue = df['money'].sum()
 print("\n--- Total Revenue ---")
+
 print("₱{:.2f}".format(total_revenue))
